2cd282313cd0f8989e4ad2c853c9995eb4a295e7
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-10-08 13:06:40 -0300

    adding enterprise member

--- sdpm/apps/enterprises/models.py
+++ sdpm/apps/enterprises/models.py
@@ -8,6 +8,8 @@
     :copyright: (c) 2012 by Arruda.
 """
 from django.db import models
+from model_utils import Choices
+
 from utils.abs_models import Abs_Named_Model
 
 class Enterprise(Abs_Named_Model):
@@ -15,3 +17,22 @@ class Enterprise(Abs_Named_Model):
     
     class Meta:
         app_label = 'enterprises'
+
+
+
+class EnterpriseMember(models.Model):
+    "A member from a given Enterprise"
+    MEMBER_TYPE = Choices(
+                            (0, 'owner', 'Owner'),
+                            (1, 'member', 'Member'),
+                            )
+    
+    user = models.ForeignKey('auth.User')    
+    enterprise = models.ForeignKey(Enterprise, related_name='members')
+    
+    member_type = models.IntegerField(choices=MEMBER_TYPE, default=MEMBER_TYPE.member)
+    
+    def __unicode__(self):
+        return "% (% - %) " % (self.user.username, self.enterprise.name, self.member_type) 
+    
+    
