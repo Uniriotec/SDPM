4e620d3cf6795d25f7b1ac8f9831ccb9001527e4
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-10-08 12:46:23 -0300

    clearing fixtures, adding sample model

--- sdpm/fixtures/carregar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./manage.py loaddata fixtures/admin_user.yaml
-./manage.py loaddata fixtures/users.yaml
-./manage.py loaddata fixtures/players.yaml
-./manage.py runscript create_daemon_rpg_test
-
