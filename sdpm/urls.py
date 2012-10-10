eda7000d7c3e53bf6f2ad67ea4a052a1c1fdd723
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-10-09 22:55:48 -0300

    again

--- sdpm/urls.py
+++ sdpm/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.views.generic.simple import direct_to_template
 from django.conf import settings
@@ -22,9 +23,12 @@ urlpatterns = patterns('',
 #)
 
 if settings.SERVE_MEDIA:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$', 
-            'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, }),
-        url(r'^static/(?P<path>.*)$', 
-            'django.views.static.serve', {'document_root': settings.STATIC_ROOT, }),
-    )
+#    urlpatterns += patterns('',
+#        url(r'^media/(?P<path>.*)$', 
+#            'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, }),
+#        url(r'^static/(?P<path>.*)$', 
+#            'django.views.static.serve', {'document_root': settings.STATIC_ROOT, }),
+#    )
+    
+    
+    urlpatterns += staticfiles_urlpatterns()
