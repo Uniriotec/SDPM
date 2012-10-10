0562080068396c9147906ec2744011e3a9b9384a
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-10-09 22:48:45 -0300

    adding static to server media

--- sdpm/urls.py
+++ sdpm/urls.py
@@ -25,4 +25,6 @@ if settings.SERVE_MEDIA:
     urlpatterns += patterns('',
         url(r'^media/(?P<path>.*)$', 
             'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, }),
+        url(r'^static/(?P<path>.*)$', 
+            'django.views.static.serve', {'document_root': settings.STATIC_ROOT, }),
     )
