d20e1fc6951e377619052a90676dbbb2c3d65946
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-10-16 16:35:07 -0300

    trying to put another server as the media server

--- sdpm/settings/env_prod.py
+++ sdpm/settings/env_prod.py
@@ -4,7 +4,11 @@ SITE_ID = 1
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-SERVE_MEDIA = True
+SERVE_MEDIA = False
+
+MEDIA_SERVER_URL = 'http://sdpm.arruda.blog.br'
+MEDIA_URL = MEDIA_SERVER_URL+'/media/'
+STATIC_URL = MEDIA_SERVER_URL+'/static/'
 
 DATABASES = {
              'default': dj_database_url.config(default='postgres://localhost')
