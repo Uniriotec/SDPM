3f4f06273881336b4ea88b3aeb3ab77e9bd8324c
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-10-17 10:09:45 -0300

    still doing the reg form

--- sdpm/apps/enterprises/forms.py
+++ sdpm/apps/enterprises/forms.py
@@ -9,19 +9,29 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
+from django.contrib.auth.models import User
+
 class RegistrationForm(forms.Form):
     "Form that register a enterprise and a owner user for it"
-    enterprise = forms.CharField(max_length=100)
+    enterprise = forms.CharField(label=_("Enterprise's Name")max_length=100)
     
     owner_email = forms.EmailField(label=_("Owner's Email"))
-    passowrd = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+    passowrd1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput,
+        help_text = _("Enter the same password as above, for verification."))
     
     
     def clean_owner_email(self):
         data = self.cleaned_data['owner_email']
-        if "fred@example.com" not in data:
-            raise forms.ValidationError("You have forgotten about Fred!")
+        if User.objects.filter(email=data.lower()) != []:
+            raise forms.ValidationError(_("This email is already been used"))
 
-        # Always return the cleaned data, whether you have changed it or
-        # not.
         return data
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1", "")
+        password2 = self.cleaned_data["password2"]
+        if password1 != password2:
+            raise forms.ValidationError(_("The two password fields didn't match."))
+        return password2
+    
--- sdpm/apps/enterprises/views.py
+++ sdpm/apps/enterprises/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth import login
 from django.shortcuts import redirect, get_object_or_404
 from annoying.decorators import render_to
 
+from django.contrib.auth.models import User
+
 from enterprises.forms import RegistrationForm
 
-from django.contrib.auth.models import User
+from enterprises.models import Enterprise, EnterpriseMember
 
 @render_to('user/register.html')
 def register(request): 
@@ -14,8 +16,27 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid(): 
+            #Create the user
+            owner_email = form.cleaned_data.get('owner_email')
+            passwd = form.cleaned_data.get('password1')
             
-            return redirect('/thanks/')
+            user = User(email=owner_email)
+            user.set_password(passwd)            
+            user.save()
+            user.username = user.pk
+            user.save()
+            
+            #create the enterprise
+            ep_name = form.cleaned_data.get('enterprise')
+            ep = Enterprise(name=ep_name)
+            ep.save()
+            
+            #create the enterprise member
+            ep_member = EnterpriseMember(user=user,enterprise=ep)
+            ep_member.member_type = EnterpriseMember.MEMBER_TYPE.owner
+            ep_member.save()
+            
+            return redirect('/')
     else:
         form = RegistrationForm()
 
